67ba952afa31536ebb030574a8ee81e37907f110
nillchik <daniilzhitko22@mail.ru>
2025-12-30 14:53:24 +0300

    add color extension + fix name

---
+++ FoodApp/App/Helpers/Color+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extensions.swift
+//  FoodApp
+//
+//  Created by split on 29/12/2025.
+//
+
+import SwiftUI
+
+struct Color_Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color_Extensions()
+}
